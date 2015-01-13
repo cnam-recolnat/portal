2a4ab3bb1861d6fe7c5c60cc1e7531e8f4ca033a
Arié Bénichou <arie.benichou@gmail.com>
2015-01-13 19:36:51 +0100

    added useful instructions once service "www" has started

--- services/www/runme.sh
+++ services/www/runme.sh
@@ -4,3 +4,5 @@ docker rm static
 docker run --name static -d -p 3000:3000 -p 2222:22 -v ~/.ssh:/root/.ssh static
 docker ps
 docker logs static
+
+echo you can now open your browser at : http://163.173.197.69:3000/
