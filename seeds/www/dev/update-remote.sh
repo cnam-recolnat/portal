de882b857015362731074fbe66ff9f696365f966
Arié Bénichou <arie.benichou@gmail.com>
2015-01-19 12:49:06 +0100

    fixed rsync destination

--- seeds/www/dev/update-remote.sh
+++ seeds/www/dev/update-remote.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #TODO encapsulate variance
-rsync -rtlvazPC --delete-after --force --stats . -e "ssh -p 2222" root@choupinette:~/portal/seed/www/
+rsync -rtlvazPC --delete-after --force --stats . -e "ssh -p 2222" root@choupinette:/root/portal/seeds/www/
