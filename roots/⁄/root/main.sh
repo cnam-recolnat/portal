46b9d34b39a0011b450a8ff9dc1ba38f8bab9b55
Arié Bénichou <arie.benichou@gmail.com>
2015-01-21 18:04:21 +0100

    testing reload command

--- roots/⁄/root/main.sh
+++ roots/⁄/root/main.sh
@@ -7,6 +7,7 @@
 service haproxy status
 service haproxy start
 service haproxy status
+ps aux | grep haproxy
 #-----------------------------------------------------------8<----------------------------------------------------------
 cd etcd-v2.0.0-rc.1-linux-amd64
 ./etcd --version
@@ -16,10 +17,7 @@ sleep 5
 #-----------------------------------------------------------8<----------------------------------------------------------
 nohup curl -L http://127.0.0.1:4001/v2/keys/myapp?wait=true\&recursive=true &
 #-----------------------------------------------------------8<----------------------------------------------------------
-which confd
-confd -onetime -backend etcd -node 127.0.0.1:4001
-#-----------------------------------------------------------8<----------------------------------------------------------
-sleep 10
+sleep 5
 #-----------------------------------------------------------8<----------------------------------------------------------
 curl -L -X PUT http://127.0.0.1:4001/v2/keys/myapp/database/url -d value="db.example.com"
 curl -L -X PUT http://127.0.0.1:4001/v2/keys/myapp/database/user -d value="rob"
@@ -27,6 +25,9 @@ curl -L -X PUT http://127.0.0.1:4001/v2/keys/myapp/database/user -d value="rob"
 curl -L http://127.0.0.1:4001/v2/keys/myapp/database/url
 curl -L http://127.0.0.1:4001/v2/keys/myapp/database/user
 #-----------------------------------------------------------8<----------------------------------------------------------
+which confd
+confd -onetime -backend etcd -node 127.0.0.1:4001
+ps aux | grep haproxy
 cat /tmp/myconfig.conf
 #-----------------------------------------------------------8<----------------------------------------------------------
 echo "root:abracadabra" | chpasswd
